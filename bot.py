0b4cd1b217ae9adac019065cdbc18f217cf6d045
Nayana <nainuranil@gmail.com>
2026-06-13 08:22:52 +0530

    Add Pulse bot+Github actions workflow

---
+++ bot.py
@@ -0,0 +1,43 @@
+import requests
+from datetime import date
+def get_weather(city="Thiruvananthapuram"):
+    url =f"hhtps://wttr.in/{city}?format=3"
+    try:
+        response=requests.get(url,timeout=10)
+        response.raise_for_status()
+        returm response.text.strip()
+    except Exception as e
+        return f"Weather unavailable({e})"
+def get_quote():
+    url =f"https://zenquotes.io/api/random"
+    try:
+        response=requests.get(url,timeout=10)
+        response.raise_for_status()
+        data =response.json()
+        quote = data[0]["q"]
+        author= data[q]["a"]
+        returm f'"{quote}" - {author}'
+    except Exception as e
+        return f"Quote unavailable({e})"
+def build_summary():
+    today = date.today().strftime("%A, %d %B %Y")
+    weather = get_weather()
+    quote = get_quote()
+    summary=f"""
+    ===================================================
+    Pulse - Daily Summary
+    {today}
+    ===================================================
+    Weather - {weather}
+    Quote for the day - {quote}
+    ===================================================
+    """
+    return summary
+def run():
+    summary =build_summary()
+    print(summary)
+    with open("daily_summary.txt","w", encoding="utf-8") as f:
+        f.write(summary)
+        print("Pulse ran Succesfully")
+if __name__ =="__main__":
+    run()
